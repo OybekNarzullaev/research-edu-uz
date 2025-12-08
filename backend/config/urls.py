2ef952333117efd4b809742d062ec34f2f05831f
OybekNarzullaev <oybeknarzullaev99@gmail.com>
2025-12-08 16:13:12 +0500

    test

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,4 +11,9 @@ urlpatterns = [
     path('api/conferences/', include('conferences.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(
+        r'^files/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+]
