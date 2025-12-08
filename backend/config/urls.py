07c93571d2a5cef8440e052e107ca6d9c9cc6b67
OybekNarzullaev <oybeknarzullaev99@gmail.com>
2025-12-08 16:16:48 +0500

    test

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,19 +1,18 @@
-
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/conferences/', include('conferences.urls')),
 ]
 
+# Production security + PDF serve
 urlpatterns += [
     re_path(
-        r'^files/(?P<path>.*)$', serve,
-        {'document_root': settings.MEDIA_ROOT}
+        r'^files/(?P<path>[\w./-]+)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': False}
     ),
 ]
